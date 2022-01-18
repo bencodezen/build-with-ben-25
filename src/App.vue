b13b09cf9b2c55b627def326fd24a6be41a9ac68
Ben Hong <ben@bencodezen.io>
2022-01-18 12:22:10 -0500

    feature: add basic scaffold for anime show tracker

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,55 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { computed, ref } from 'vue'
+
+const newShow = ref('')
+const newShowStatus = ref('watching')
+
+const animeShows = ref([])
+
+const currentlyWatchingShows = computed(() => {
+  return animeShows.value.filter(show => show.status === 'watching')
+})
+
+const watchedShows = computed(() => {
+  return animeShows.value.filter(show => show.status === 'watched')
+})
+
+const addShow = () => {
+  animeShows.value.push({
+    show: newShow.value,
+    status: newShowStatus.value
+  })
+  newShow.value = ''
+  newShowStatus.value = 'watching'
+}
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <main>
+    <h1>Anime Show Tracker</h1>
+    <form @submit.prevent>
+      <input type="text" placeholder="Show Name" v-model="newShow" />
+      <select name="show-status" id="show-status" v-model="newShowStatus">
+        <option value="watching" selected>Watching</option>
+        <option value="watched">Watched</option>
+      </select>
+      <button @click="addShow">Add Show</button>
+    </form>
+    <section class="anime-list">
+      <h2>Currently Watching</h2>
+      <ul>
+        <li v-for="show in currentlyWatchingShows">
+          {{ show }}
+        </li>
+      </ul>
+    </section>
+    <section class="anime-list">
+      <h2>Watched</h2>
+      <li v-for="show in watchedShows">
+        {{ show }}
+      </li>
+    </section>
+  </main>
 </template>
 
 <style>
@@ -16,6 +59,10 @@ import HelloWorld from './components/HelloWorld.vue'
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding: 30px;
+}
+
+.anime-list {
+  text-align: left;
 }
 </style>
